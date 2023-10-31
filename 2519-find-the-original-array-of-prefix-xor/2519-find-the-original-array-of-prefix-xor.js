d10889a5011260a4f69b59876059d602ff7050c2
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2023-10-31 06:27:14 +0530

    Create 2519-find-the-original-array-of-prefix-xor.js

---
+++ 2519-find-the-original-array-of-prefix-xor/2519-find-the-original-array-of-prefix-xor.js
@@ -0,0 +1,7 @@
+var findArray = function(pref) {
+        let result = [pref[0]];
+        for(let i = 1; i < pref.length; i++) {
+            result.push(pref[i] ^ pref[i-1]);
+        }
+        return result;
+    }
