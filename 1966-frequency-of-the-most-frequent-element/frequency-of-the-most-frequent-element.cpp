c52c2f031fb1d6faaa5fd968cd2c7f996c3f7831
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2023-11-18 07:49:01 +0530

    Time: 124 ms (97.80%) | Memory: 99.5 MB (25.44%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,16 +1,28 @@
+#pragma GCC optimize("O3")
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        long int i = 0, j = 0, sum = 0, l = 0, tsum = 0;
-        for (i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            while ((i - j + 1) * nums[i] - sum > k) {
-                sum -= nums[j];
-                j++;
+        int n=nums.size();
+        int freq=0, l=0;
+        long long sum=0;
+        #pragma unroll
+        for(int r=0; r<n; r++){
+            long long x=nums[r];
+            sum+=x;
+            #pragma unroll
+            while((r-l+1)*x-sum>k){
+               sum-=nums[l++]; 
             }
-            l = max(l, i - j + 1);
-        }
-        return l;
+            freq=max(freq, r-l+1);
+        }       
+        return freq;
     }
 };
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
