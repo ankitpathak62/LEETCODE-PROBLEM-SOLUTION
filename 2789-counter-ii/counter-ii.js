d578ca93b30a301effb0d21b00096de05bb42158
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2023-11-19 12:09:55 +0530

    Time: 62 ms (35.53%) | Memory: 44.4 MB (66.00%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -1,25 +1,19 @@
-class Counter {
-  constructor(init) {
-    this.init = init;
-    this.presentCount = init;
-  }
-
-  increment() {
-    this.presentCount += 1;
-    return this.presentCount;
-  }
-
-  decrement() {
-    this.presentCount -= 1;
-    return this.presentCount;
-  }
-
-  reset() {
-    this.presentCount = this.init;
-    return this.presentCount;
-  }
-}
-
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
 var createCounter = function(init) {
-  return new Counter(init);
+    let v = init
+    return {
+        increment:()=> ++v,
+        decrement:()=> --v,
+        reset:()=> v = init
+    }
 };
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
