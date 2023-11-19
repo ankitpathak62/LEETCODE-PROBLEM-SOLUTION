4db21c340dae465210a42d02ce9c29d4c7ad11f3
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2023-11-19 12:09:46 +0530

    Time: 51 ms (90.14%) | Memory: 44.6 MB (55.95%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,25 @@
+class Counter {
+  constructor(init) {
+    this.init = init;
+    this.presentCount = init;
+  }
+
+  increment() {
+    this.presentCount += 1;
+    return this.presentCount;
+  }
+
+  decrement() {
+    this.presentCount -= 1;
+    return this.presentCount;
+  }
+
+  reset() {
+    this.presentCount = this.init;
+    return this.presentCount;
+  }
+}
+
+var createCounter = function(init) {
+  return new Counter(init);
+};
