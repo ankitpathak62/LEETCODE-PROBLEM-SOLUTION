8334fd8a4b8b6d70b0aa1e350bf39c7b87cb5395
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2023-11-19 12:02:18 +0530

    Time: 926 ms (14.95%) | Memory: 24.1 MB (39.18%) - LeetSync

---
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reductionOperations(self, nums):
+        n = len(nums)
+        freq = [0] * 50001
+        for num in nums:
+            freq[num] += 1
+        res, operations = 0, 0
+        for i in range(50000, 0, -1):
+            if freq[i] > 0:
+                operations += freq[i]
+                res += operations - freq[i]
+        return res
+        
