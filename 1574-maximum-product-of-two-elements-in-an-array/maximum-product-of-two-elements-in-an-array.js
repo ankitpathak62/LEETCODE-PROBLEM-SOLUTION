890e5e87d9cd647d39d00c497ae0607abf8bf63e
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2023-12-12 07:51:06 +0530

    Time: 58 ms (40.98%) | Memory: 42.8 MB (45.30%) - LeetSync

---
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
@@ -0,0 +1,15 @@
+var maxProduct = function(nums) {
+    let max1 = Number.MIN_SAFE_INTEGER;
+    let max2 = Number.MIN_SAFE_INTEGER;
+
+    for (const num of nums) {
+        if (num >= max1) {
+            max2 = max1;
+            max1 = num;
+        } else if (num > max2) {
+            max2 = num;
+        }
+    }
+
+    return (max1 - 1) * (max2 - 1);
+};
