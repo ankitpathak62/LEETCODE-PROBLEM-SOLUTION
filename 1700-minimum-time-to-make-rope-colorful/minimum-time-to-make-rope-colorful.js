aede1c5c758689871cc6ceb8c9cb476beaa89bd2
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2023-12-27 20:44:40 +0530

    Time: 88 ms (40.82%) | Memory: 52.4 MB (89.80%) - LeetSync

---
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.js
@@ -0,0 +1,22 @@
+var minCost = function(colors, neededTime) {
+    let totalTime = 0;
+    let i = 0;
+    let j = 0;
+
+    while (i < neededTime.length && j < neededTime.length) {
+        let currTotal = 0;
+        let currMax = 0;
+
+        while (j < neededTime.length && colors[i] === colors[j]) {
+            currTotal += neededTime[j];
+            currMax = Math.max(currMax, neededTime[j]);
+            j++;
+        }
+
+        totalTime += currTotal - currMax;
+        i = j;
+    }
+
+    return totalTime;
+};
+
