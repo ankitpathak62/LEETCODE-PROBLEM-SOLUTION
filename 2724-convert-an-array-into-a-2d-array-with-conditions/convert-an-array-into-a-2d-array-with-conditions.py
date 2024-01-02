3add129305aae9883a1f1ae7e2af632d41fcb082
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2024-01-02 10:03:47 +0530

    Time: 75 ms (5.01%) | Memory: 17.3 MB (12.28%) - LeetSync

---
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMatrix(self, v: List[int]) -> List[List[int]]:
+        um = {}
+        for i in v:
+            um[i] = um.get(i, 0) + 1
+        
+        ans = []
+        while um:
+            temp = []
+            to_erase = []
+            for f, s in um.items():
+                temp.append(f)
+                s -= 1
+                if s == 0:
+                    to_erase.append(f)
+                um[f] = s
+            ans.append(temp)
+            for i in to_erase:
+                del um[i]
+        return ans
