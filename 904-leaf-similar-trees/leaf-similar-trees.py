7e7b92d9440c9c77b047cc924f852e5a44ff72ed
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2024-01-09 17:03:55 +0530

    Time: 39 ms (63.48%) | Memory: 17.5 MB (7.47%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -0,0 +1,17 @@
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def collect_leaf_values(root, leaf_values):
+            if not root:
+                return
+            if not root.left and not root.right:
+                leaf_values.append(root.val)
+            collect_leaf_values(root.left, leaf_values)
+            collect_leaf_values(root.right, leaf_values)
+
+        leaf_values1 = []
+        leaf_values2 = []
+
+        collect_leaf_values(root1, leaf_values1)
+        collect_leaf_values(root2, leaf_values2)
+
+        return leaf_values1 == leaf_values2
