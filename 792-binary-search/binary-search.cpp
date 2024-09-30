284c53ebee841fc07ef630bba9a393dc975c33b5
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2024-09-30 18:03:52 +0530

    Time: 28 ms (49.55%) | Memory: 30.2 MB (23.47%) - LeetSync

---
+++ 792-binary-search/binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int Element) {
+        int n = arr.size();
+        int first_element = 0;
+        int last_element = n - 1;
+        int mid;
+
+        while (first_element <= last_element) {
+            mid = (first_element + last_element) / 2;
+
+            if (arr[mid] == Element) {
+                return mid;
+            } else if (arr[mid] < Element) {
+                first_element = mid + 1;
+            } else {
+                last_element = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
