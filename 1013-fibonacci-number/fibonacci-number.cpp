02e2d2aa32ab59d29e96edc2b5cf3eb60be3df40
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2025-07-02 07:41:14 +0530

    Time: 9 ms (49.70%) | Memory: 7.7 MB (87.61%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        //base case
+        if (n==1){
+            return 1;
+        }
+        if(n==0){
+            return 0;
+        }
+
+        return fib(n-1)+fib(n-2);
+        
+    }
+};
