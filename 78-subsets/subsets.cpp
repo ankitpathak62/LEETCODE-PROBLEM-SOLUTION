c73cd38c0a204de324689d9aa886e4a7095a4de9
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2025-07-25 12:21:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (30.52%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void subsequence(vector<int>&arr, int n, int index, vector<int> temp,
+    void subsequence(vector<int>&arr, int n, int index, vector<int>& temp,
                      vector<vector<int>>& ans) {
         // base case
         if (index == n) {
@@ -12,6 +12,7 @@ public:
         // Take the element
         temp.push_back(arr[index]);
         subsequence(arr, n, index + 1, temp, ans);
+        temp.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
