87e7c70ef82de6a351bc3f748101c0a98441febc
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2025-07-25 12:20:13 +0530

    Time: 3 ms (32.61%) | Memory: 15.7 MB (16.23%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void subsequence(vector<int>&arr, int n, int index, vector<int> temp,
+                     vector<vector<int>>& ans) {
+        // base case
+        if (index == n) {
+            ans.push_back(temp);
+            return;
+        }
+        // Not taking the element
+        subsequence(arr, n, index + 1, temp, ans);
+        // Take the element
+        temp.push_back(arr[index]);
+        subsequence(arr, n, index + 1, temp, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        vector<vector<int>> ans;
+        subsequence(nums, nums.size(), 0, temp, ans);
+        return ans;
+    }
+};
