41dec1d5f9234ae95d22928c9942c4950b7c5da4
Ankit Pathak <99532574+ankitpathak62@users.noreply.github.com>
2025-07-31 14:49:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (47.24%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    void permutation(vector<int>&arr, int size, vector<int>& temp,
-                     vector<vector<int>>& ans, vector<bool>& visited) {
+    void permutation(vector<int>&arr,  vector<vector<int>>& ans,int index) {
         // base case
-        if (temp.size() == size) {
-            ans.push_back(temp);
+        if (arr.size() == index) {
+            ans.push_back(arr);
             return;
         }
 
-        for (int i = 0; i < size; i++) {
-            if (visited[i] == 0) {
-                visited[i] = 1;
-                temp.push_back(arr[i]);
-                permutation(arr, size, temp, ans, visited);
-                visited[i] = 0;
-                temp.pop_back();
-            }
+        for (int i = index; i < arr.size(); i++) {
+             swap(arr[index],arr[i]);
+             permutation(arr,ans,index+1);
+             swap(arr[index],arr[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
 
         vector<vector<int>> ans;
-        int size = nums.size();
-        vector<int> temp;
-        vector<bool> visited(nums.size(), 0);
-        permutation(nums, size, temp, ans, visited);
+         
+        permutation(nums,   ans ,0);
         return ans;
     }
 };
